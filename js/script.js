3eb65f127015a9bcb9a6999c0cb8ab3b9a9ce61d
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-22 17:12:03 +0100

    [feat] get 10 email from api and push in an external array

--- js/script.js
+++ js/script.js
@@ -6,9 +6,34 @@
 // Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre) 
 
 
-
 // mi ricavo i dati del Dom
+
+const BoxEmail = document.getElementById('box-email')
 // mi salvo l'endpoint
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 // creo un ciclo di misura 10 
+
     // mi creo la chiamata ricavandomi il datoiesimo che mi interessa 
-    // lo stampo a schermo
+let emails = [];
+    for(let i = 0; i < 10; i++){
+        axios.get(endpoint)
+            .then((element) => {
+                // console.log(element);
+                const data = element.data;
+                // console.log(data);
+                const email = data.response
+                // console.log(email);
+
+                // mi salvo i dati in un array esterno
+                array.push(email)  
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+
+    }
+console.log(array);
+
+
+// lo stampo a schermo
+// BoxEmail.innerHTML = `<li class="list-group-item">${email}</li>`
