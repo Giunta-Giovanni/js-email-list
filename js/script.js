196f6394ebeca3f51276b8b223be76ff78d548c0
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-22 16:36:59 +0100

    [feat] general Human code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// # js-email-list
+// Attraverso l’apposita API di Boolean  https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+// Bonus
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre) 
+
+
+
+// mi ricavo i dati del Dom
+// mi salvo l'endpoint
+// creo un ciclo di misura 10 
+    // mi creo la chiamata ricavandomi il datoiesimo che mi interessa 
+    // lo stampo a schermo
