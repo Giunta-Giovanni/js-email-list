0902af5905509685411f672e22260a46caeea176
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-22 18:04:04 +0100

    [feat] alternative method

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 const boxEmail = document.getElementById('box-email')
 // mi salvo l'endpoint
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
-
+/*
 // mi salvo un array vuoto che contiene 
 let emails = [];
 // creo un ciclo di misura 10 
@@ -27,6 +27,7 @@ for(let i = 0; i < 10; i++){
                 const result = data.response
                 // console.log(emails);
 
+                
                 // mi salvo i dati in un array esterno
                 emails.push(result);
 
@@ -51,6 +52,40 @@ for(let i = 0; i < 10; i++){
                 }
             })
             .catch((error) => {
+                // codice da eseguire in caso di errore
+                console.error(error)
+            })
+    }
+*/
+
+// metodo senza array esterno
+// mi salvo l'elemento che conterra la stringa html inizialmente vuota
+
+let items = ``;
+// creo un ciclo di misura 10 
+for(let i = 0; i < 10; i++){
+
+    // mi creo la chiamata ricavandomi il datoiesimo che mi interessa 
+        axios.get(endpoint)
+            .then((element) => {
+                // mi salvo data dove all'interno ci sara l'informazione che mi serve
+                const data = element.data;
+
+                // mi ricavo l'informazione
+                const email = data.response
+
+                // generami una stringa
+                let item = `<li class="list-group-item">${email}</li>`;
+
+                // aggiungila al resto 
+                items += item;
+
+                // lo stampo a schermo
+                boxEmail.innerHTML = items
+                })
+
+            .catch((error) => {
+                // codice da eseguire in caso di errore
                 console.error(error)
             })
     }
